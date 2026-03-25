f6e4a3b043ef746eab11a1a7efb2147098b1925f
XRockKimu <kimkheng.ly@student.cadt.edu.kh>
2026-03-26 02:27:06 +0700

    W9-01–library_screen

--- lib/ui/widgets/song/song_tile.dart
+++ lib/ui/widgets/song/song_tile.dart
@@ -21,9 +21,10 @@ class SongTile extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(15)
+          borderRadius: BorderRadius.circular(15),
         ),
         child: ListTile(
+          leading: CircleAvatar(backgroundImage: NetworkImage(song.imageUrl)),
           onTap: onTap,
           title: Text(song.title),
           trailing: Text(
